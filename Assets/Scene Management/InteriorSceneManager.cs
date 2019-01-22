fdba9673dc2f61baae0dea78b448b1355352ee4b
Linus Waddell <linuswaddell@gmail.com>
2019-01-21 20:39:34 -0800

    Add library to store references to scene portals

--- Assets/Scene Management/InteriorSceneManager.cs
+++ Assets/Scene Management/InteriorSceneManager.cs
@@ -4,19 +4,10 @@ using UnityEngine;
 
 public class InteriorSceneManager : MonoBehaviour {
 
-	// * * * This class was meant to store a list of scriptable objects containing scene data.
-	// * * * This approach has since been abandoned.
-
-
-	/*static InteriorSceneManager instance;
-	// Scene list is populated via the inspector, for now
-	[SerializeField] List<InteriorSceneData> interiorScenes;
-
 	void Start () {
-		instance = this;
+		SceneLoader.OnScenesLoaded += OnScenesLoaded;
 	}
-	
-	public static List<InteriorSceneData> GetInteriorSceneList () {
-		return instance.interiorScenes;
-	}*/
+	void OnScenesLoaded () {
+		ScenePortalLibrary.BuildLibrary ();
+	}
 }
---
+++ Assets/Scene Management/ScenePortalLibrary.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenePortalLibrary : MonoBehaviour
+{
+	static List<ScenePortal> library;
+	public static List<ScenePortal> Library {get{return library;}}
+
+	public static void BuildLibrary () {
+		library = new List<ScenePortal> ();
+		foreach (ScenePortal portal in GameObject.FindObjectsOfType<ScenePortal>()) {
+			library.Add (portal);
+		}
+	}
+	public static List<ScenePortal> GetPortalsBetweenScenes (string startScene, string destScene) {
+		List<ScenePortal> results = new List<ScenePortal> ();
+		foreach (ScenePortal portal in library) {
+			if (portal.gameObject.scene.name == startScene && portal.DestinationScene == destScene) {
+				results.Add(portal);
+			}
+		}
+		return results;
+	}
+}
