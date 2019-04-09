8cb441ca36f404a993b0b00dee1dd855875e1e53
Linus Waddell <linuswaddell@gmail.com>
2019-04-08 23:03:17 -0700

    Soundtrack playback functionality

---
+++ Assets/SoundtrackPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundtrackPlayer : MonoBehaviour
+{
+	[SerializeField] AudioClip soundtrack;
+    // Start is called before the first frame update
+    void Start()
+    {
+		AudioSource audioSource = gameObject.AddComponent<AudioSource> ();
+		audioSource.clip = soundtrack;
+		audioSource.spatialBlend = 0f;
+		audioSource.loop = true;
+		audioSource.volume = 0.4f;
+		audioSource.Play ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
