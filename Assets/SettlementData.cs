d03e0849e616d51a5f14039bc256cc7e63547b84
linus <linuswaddell@gmail.com>
2019-08-13 14:41:32 -0700

    Classes for settlement management

---
+++ Assets/SettlementData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettlementData
+{
+
+	string settlementId;
+	string settlementName;
+
+	string leaderActorId;
+	List<string> memberActorIds;
+
+	public SettlementData(string id, string name, string leaderId) : this(id, name, leaderId, new List<string>()) { }
+	public SettlementData(string id, string name, string leaderId, List<string> memberIds)
+	{
+		this.settlementId = id;
+		this.settlementName = name;
+		this.leaderActorId = leaderId;
+		this.memberActorIds = memberIds;
+	}
+}
---
+++ Assets/SettlementManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Stores all settlements in the world, and handles accessing them
+public class SettlementManager : MonoBehaviour
+{
+	static List<SettlementData> settlements;
+
+
+	/// <returns>The unique ID of the newly created settlement.</returns>
+	public static string CreateSettlement (string name, string leaderId)
+	{
+		// TDOD make sure settlement IDs are unique
+		string id = name.Replace(' ', '_').ToLower();
+		SettlementData settlement = new SettlementData(id, name, leaderId);
+		settlements.Add(settlement);
+		return id;
+	}
+}
