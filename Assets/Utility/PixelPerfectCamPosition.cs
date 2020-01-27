fdd4fe1646027e4e8963780e9699e2261c4fe840
linus <24817932+linuswaddell@users.noreply.github.com>
2020-01-26 23:53:38 -0800

    adjust camera size and position to reduce pixel distortion

---
+++ Assets/Utility/PixelPerfectCamPosition.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Adjusts the position of this camera so that all screen pixels fit perfectly within game pixels
+// (assuming the camera size divides nicely into the screen resolution)
+public class PixelPerfectCamPosition : MonoBehaviour
+{
+	private const int PIXELS_PER_UNIT = 16;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+		float newLocalX = GetClosestSnapPosition(Screen.height, Camera.main.orthographicSize).x - (transform.position.x - transform.localPosition.x);
+		float newLocalY = GetClosestSnapPosition(Screen.height, Camera.main.orthographicSize).y - (transform.position.y - transform.localPosition.y);
+		this.transform.localPosition = new Vector3 (newLocalX, newLocalY, this.transform.localPosition.z);
+    }
+
+	private Vector3 GetClosestSnapPosition (int resY, float camSize)
+	{
+		float newX;
+		float newY;
+		// Get the x and y ignoring local transform
+		Vector3 actualPos = new Vector3(transform.position.x - transform.localPosition.x, transform.position.y - transform.localPosition.y, transform.position.z);
+		float movementIncrement = CalculateIncrement(resY, camSize);
+
+		// Set each new position component to the highest multiple of the increment less than the actual position
+		newX = actualPos.x - (actualPos.x % movementIncrement);
+		newY = actualPos.y - (actualPos.y % movementIncrement);
+
+		// If either component is closer to the next highest multiple, add the increment
+		if (actualPos.x % movementIncrement > movementIncrement / 2)
+		{
+			newX += movementIncrement;
+		}
+		if (actualPos.y % movementIncrement > movementIncrement / 2)
+		{
+			newY += movementIncrement;
+		}
+
+		return new Vector3(newX, newY, actualPos.z);
+	}
+
+	// How many units the camera should move at a time
+	private float CalculateIncrement (int resY, float camSize)
+	{
+		return (1 / GetPixelSize(resY, camSize)) / PIXELS_PER_UNIT;
+	}
+
+	// Returns the width of a game pixel in real screen pixels
+	private float GetPixelSize (int resY, float camSize)
+	{
+		float result = resY / (camSize * 2 * PIXELS_PER_UNIT);
+		return result;
+	}
+}
---
+++ Assets/Utility/PixelPerfectCamSize.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class PixelPerfectCamSize : MonoBehaviour
+{
+	// The ideal camera orthographic size, which we'll try to find values close to
+	[SerializeField] private float referenceSize = 8.4375f;
+
+	private const int PIXELS_PER_UNIT = 16;
+
+	private Camera thisCam;
+	private float lastRecordedResolution;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		thisCam = GetComponent<Camera>();
+		if (thisCam == null)
+		{
+			Debug.LogError("This script needs a camera!");
+		}
+		thisCam.orthographicSize = FindBestSize(Screen.height);
+		lastRecordedResolution = Screen.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		// If the screen dimensions have changed, recalculate the camera size
+        if (Screen.height != lastRecordedResolution)
+		{
+			thisCam.orthographicSize = FindBestSize(Screen.height);
+		}
+		lastRecordedResolution = Screen.height;
+    }
+
+	// Finds an orthographic camera size close to the reference that provides an
+	// exact integer pixels-per-unit
+	private float FindBestSize (int resY)
+	{
+		float approxPixelsPerPixel = (resY / 2) / (referenceSize * PIXELS_PER_UNIT);
+		float newPixelsPerPixel = Mathf.RoundToInt(approxPixelsPerPixel);
+		if (newPixelsPerPixel < 1)
+		{
+			// If rounding would take us to zero, just keep the original pixel density
+			newPixelsPerPixel = approxPixelsPerPixel;
+		}
+		return (resY / 2) / (newPixelsPerPixel * PIXELS_PER_UNIT);
+	}
+}
