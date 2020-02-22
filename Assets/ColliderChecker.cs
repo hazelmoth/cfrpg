57b6ab9f44279f1f49d4a683dcf7f90669f54db0
linus <24817932+linuswaddell@users.noreply.github.com>
2020-02-22 12:37:12 -0800

    fix bug with obstacle detection

--- Assets/ColliderChecker.cs
+++ Assets/ColliderChecker.cs
@@ -6,13 +6,35 @@ public class CollisionChecker : MonoBehaviour
 {
 	public bool Colliding { get; private set; }
 
+	private ISet<Collider2D> collidingObjects;
+
 	private void OnTriggerEnter2D(Collider2D collider)
 	{
+		if (collidingObjects == null)
+		{
+			initSet();
+		}
+
+		collidingObjects.Add(collider);
 		Colliding = true;
 	}
 
 	private void OnTriggerExit2D(Collider2D collider)
 	{
-		Colliding = false;
+		if (collidingObjects == null)
+		{
+			initSet();
+		}
+		else if (collidingObjects.Contains(collider))
+		{
+			collidingObjects.Remove(collider);
+		}
+
+		Colliding = (collidingObjects.Count > 0);
+	}
+
+	private void initSet ()
+	{
+		collidingObjects = new HashSet<Collider2D>();
 	}
 }
