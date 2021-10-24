15e86bdb5d33af6bdd1bbd9484b6b483a76d28ab
linus <24817932+linuswaddell@users.noreply.github.com>
2021-10-24 01:27:17 -0700

    train system

---
+++ Assets/Scripts/Train.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train : MonoBehaviour
+{
+    [Serializable]
+    public class Wheel
+    {
+        public GameObject wheelObject;
+        public float radius;
+    }
+    [SerializeField] private float speed = -13f;
+    [SerializeField] private List<Wheel> wheels;
+
+    private void Update()
+    {
+        float linearDist = speed * Time.deltaTime;
+        wheels.ForEach(
+            wheel =>
+            {
+                float angularDist = Mathf.Rad2Deg * (linearDist / wheel.radius);
+                wheel.wheelObject.transform.Rotate(Vector3.forward, -angularDist, Space.Self);
+            });
+        transform.Translate(Vector3.right * linearDist);
+    }
+}
---
+++ Assets/Scripts/TrainSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// A GameObject that spawns fucking trains
+public class TrainSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject trainPrefab;
+    [SerializeField] private float trainInterval = 10f;
+    [SerializeField] private float maxTravelBeforeDestroy = 100;
+
+    private GameObject currentTrain;
+    private float lastTrainDestruction;
+
+    private void Update()
+    {
+        if (!currentTrain)
+        {
+            currentTrain = Instantiate(trainPrefab, this.transform, false);
+        }
+
+        if (Vector3.Distance(currentTrain.transform.position, transform.position) > maxTravelBeforeDestroy)
+            DestroyTrain();
+
+        if (Time.time - lastTrainDestruction > trainInterval) DestroyTrain();
+    }
+
+    private void DestroyTrain()
+    {
+        lastTrainDestruction = Time.time;
+        Destroy(currentTrain);
+        currentTrain = null;
+    }
+}
